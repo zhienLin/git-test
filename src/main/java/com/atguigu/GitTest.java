89fc33f2e4c13230b5dd7b8e37f08daf0cf5c8ec
zzy <840222408@qq.com>
2022-12-01 17:20:32 +0800

    push commit1

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello git3!");
         System.out.println("hello master!");
         System.out.println("hello hot-fixed!");
+        System.out.println("hello push!");
     }
 }
